780c6399ff3f0928a090d9964e5462117267781e
Andreas Johansson <z@zanmato.se>
2026-08-27 15:53:01 +0200

    shell: Handle the SelectionRangeChange input event

--- crates/shell/src/engine/quickjs/mod.rs
+++ crates/shell/src/engine/quickjs/mod.rs
@@ -3776,7 +3776,10 @@ impl ShellRuntime {
                     payload.set("secondary", *secondary)?;
                     payload.set("shift", *shift)?;
                 }
-                InputEvent::Change | InputEvent::Focus | InputEvent::Blur => {}
+                InputEvent::Change
+                | InputEvent::Focus
+                | InputEvent::Blur
+                | InputEvent::SelectionRangeChange { .. } => {}
             }
             handler.call::<_, ()>((
                 payload,
